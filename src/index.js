51a8b0d80e89dd4421c5b3c26181a8c8a7565603
Ansh197 <77569137+Ansh197@users.noreply.github.com>
2023-12-15 10:56:39 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.css'
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// const fname = "Ansh";
+// const lname = "Sharma";
+// const curYear = new Date().getFullYear();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+var text = "";
+
+  const curTime = new Date().getHours() ;
+  if(curTime<12)
+  {
+    text ='Good Morning';
+  }
+  else if (curTime>=12&&curTime<=18)
+  {
+    text = 'Good Evening';
+  }
+  else
+  {
+    text = 'Good Night';
+  }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <div>
+  //   <p
+  //     contentEditable="false"
+  //     spellCheck="false"
+  //    style={{color:'red'}} >{`Created by ${fname} ${lname}`}</p>
+  //   <p>{`Copyright ${curYear}`}</p>
+  //   <div>
+  //     <img alt=" " src="https://picsum.photos/200"/>
+  //     <img alt=" " src="https://picsum.photos/200"/>
+  //     <img alt=" " src="https://picsum.photos/200"/>
+  //   </div>
+  // </div>
+  <p>{text}</p>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// reportWebVitals();
